3fe362e6893bae5c83c18945585d259a76bf7300
Miguel Angel <miguel.carrillo.m@hotmail.com>
2024-04-17 12:27:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
    df1=pd.read_csv(uploaded_file)
 
+   st.subheader('Perfil gráfico de la variable medida.')
    df1 = df1.set_index('Time')
    st.line_chart(df1)
    
